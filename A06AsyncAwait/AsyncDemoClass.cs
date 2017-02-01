f29162cedbb3ba8f94b9e9cb2e2ef35784ef6634
Volker Hetzer <volker.hetzer@xitaso.com>
2017-02-01 18:34:01 +0100

    atarted the last bits

---
+++ A06AsyncAwait/AsyncDemoClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A06AsyncAwait
+{
+    public class AsyncDemoClass
+    {
+        public async Task DoSomethingAsync()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
